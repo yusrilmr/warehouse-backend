f09efb93bc77ef59b0d92e2b6a5cde8218c5e225
Raji, Yusril <yusril.raji@thermofisher.com>
2021-08-04 20:09:43 +0200

    initiate tests

---
+++ src/test/java/com/company/warehouse/ArticleRepositoryTests.java
@@ -0,0 +1,41 @@
+package com.company.warehouse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.company.warehouse.domain.Article;
+import com.company.warehouse.domain.ArticleRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ArticleRepositoryTests {
+    @Autowired
+    private TestEntityManager testEntityManager;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Test
+    public void insertArticle_ArticleDataIsComplete_ArticleIsSaved()
+    {
+        Article article = new Article("TB22222", "table leg", 5);
+        testEntityManager.persistAndFlush(article);
+        assertThat(article.getId()).isNotZero();
+    }
+
+    @Test
+    public void findByExternalId_IdIsNegative_ReturnNull() {
+
+    }
+
+    @Test
+    public void deleteCars() {
+        testEntityManager.persistAndFlush(new Article("TB22222", "table leg", 5));
+        testEntityManager.persistAndFlush(new Article("TB22223", "table top", 5));
+        articleRepository.deleteAll();
+        assertThat(articleRepository.findAll()).isEmpty();
+    }
+}
